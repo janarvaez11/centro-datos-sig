3ab68af1a2dc57c8b1cfc674fa7eecfecef03288
Ariel Narvaez - SEDEMI <asistente.auditoria1@sedemi.com>
2025-01-28 13:17:22 -0500

    View table for Orders

--- app/(routes)/Orders/components/ListOrders/columns.tsx
+++ app/(routes)/Orders/components/ListOrders/columns.tsx
@@ -1,2 +1,94 @@
 "use client"
 
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal, Pencil } from 'lucide-react'
+import { Order } from "@prisma/client"
+import { Button } from "@/components/ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import Link from 'next/link'
+import Image from "next/image"
+
+
+export const columns: ColumnDef<Order>[] = [
+    {
+        accessorKey: "profile Image",
+        header: "Profile Image",
+        cell: ({ row }) => {
+            const image = row.getValue("profileImage")
+            return (
+                <div className="px-3">
+                    <Image src={typeof image === "string" ? image : "/images/order-icon.png"}
+                        width={40} height={40} alt="Image" className="h-auto w-auto" />
+                </div>
+            )
+        }
+    },
+    {
+        accessorKey: "name",
+        header: ({ column }) => {
+            return (
+                <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+                    Orden
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: "order",
+        header: "Order",
+    },
+    {
+        accessorKey: "estado",
+        header: "Estado",
+    },
+    {
+        accessorKey: "tipoInspeccion",
+        header: "tipoInspeccion",
+    },
+    {
+        accessorKey: "fechaProgramada",
+        header: "fechaProgramada",
+    },
+    {
+        accessorKey: "procesoProduccion",
+        header: "procesoProduccion",
+    },
+    {
+        accessorKey: "especificacionProceso",
+        header: "especificacionProceso",
+    },
+    {
+        id: "actions",
+        header: "Actions",
+        cell: ({ row }) => {
+            const { id } = row.original
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Button variant="ghost" className="w-8 h-4 p-0">
+                            <span className="sr-only">
+                                Open Menu
+                            </span>
+                            <MoreHorizontal className="w-4 h-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+
+                        <Link href={'/order/${id}'}>
+                            <DropdownMenuItem>
+                                <Pencil className="w-4 h-4" mr-2 />
+                                Editar
+                            </DropdownMenuItem>
+                        </Link>
+
+                    </DropdownMenuContent>
+
+                </DropdownMenu>
+            )
+
+        }
+
+    },
+
+]
