3034895a2f3d762b3b383419e2cedf0e79bb2242
Ariel Narvaez <janarvaez11@espe.edu.ec>
2025-01-28 00:21:50 -0500

    Add conection to Sharepoint

---
+++ app/(routes)/Orders/page.tsx
@@ -0,0 +1,10 @@
+import { HeaderOrders } from "./components/HeaderOrders"
+
+export default function Orders(){
+    return (
+        <div>
+            <HeaderOrders/>
+            <p>List Orders</p>
+        </div>
+    )
+}
